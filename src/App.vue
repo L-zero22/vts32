f34f235233e4c3f877881d622a890dcb1eb203e8
L-zero22 <l17876189271@gmail.com>
2021-09-22 18:09:22 +0800

    删除多余文件

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <template>
- <router-view></router-view>
+    <header class="nav">
+
+    </header>
+
+    <router-view></router-view>
+
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
- 
+    name: 'App'
 })
 </script>
 
 <style>
-  *{
+* {
     padding: 0px;
     margin: 0px;
     box-sizing: border-box;
-  }
-  html,body{
+}
+html,
+body {
     width: 100vw;
     height: 100vh;
-  }
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height:100%;
-  width: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+    width: 100%;
+}
+.nav {
+    height: 100px;
+    width: 100%;
 }
 </style>
--- src/view/main.vue
+++ src/view/main.vue
@@ -1,57 +1,27 @@
+<!--
+    @desc 默认显示页面
+ -->
 <template>
-   <div class="main">
-    <div class="loginBox">
-        <div class="username">
+    <main class="content">
 
-        </div>
-        <div class="password">
-
-        </div>
-        <div class="code">
-
-        </div>
-    </div>
-   </div>
+    </main>
 </template>
 
 <script lang="ts"  setup>
-import {reactive} from 'vue'
+import { reactive } from 'vue'
 
-interface loginInfo{
-    username:string,
-    password:string,
-    code?:string
+interface loginInfo {
+    username: string
+    password: string
+    code?: string
 }
 
-
-
 const loginInfo = reactive<loginInfo>({
-    username:"",
-    password:"",
-    code:''
+    username: '',
+    password: '',
+    code: ''
 })
 </script>
 
 <style lang="scss" scoped>
-    .main{
-        width: 100%;
-        height: 100%;
-       
-        overflow: hidden auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: url('https://pixabay.com/zh/photos/sea-coast-rocks-beach-waves-ocean-6406047/https://cdn.pixabay.com/photo/2021/07/12/11/09/sea-6406047_960_720.jpg');
-    }
-    .loginBox{
-        width: 500px;
-        height: 300px;
-        border:1px solid  #dedede;
-        border-radius: 12px;
-    backdrop-filter: blur(12px);
-    -webkit-backdrop-filter: blur(12px);
-
-box-shadow: 0 0 22px 2px brown;
-
-    }
 </style>
